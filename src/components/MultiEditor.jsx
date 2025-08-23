1159cab506be39ce466ae7d39f0b9c5a07e53904
Cruduper <briefcasemanx@gmail.com>
2025-08-23 00:26:24 -0700

    adds functionality to switch between editor types in MultiEditor

--- src/components/MultiEditor.jsx
+++ src/components/MultiEditor.jsx
@@ -1,9 +1,33 @@
+import React, { useState, useEffect} from 'react';
 import ProseMirrorEditor from './ProseMirrorEditor/ProseMirrorEditor';
+import TipTapEditor from './TipTapEditor/TipTapEditor';
 
 function MultiEditor() {
+  const [editorType, setEditorType] = useState('TipTap'); // "TipTap", "Markdown-it", "ProseMirror"
+
+  function getPreviewElement() {
+    if (editorType === 'TipTap') {
+      return <TipTapEditor/>
+    }
+    else if (editorType === 'ProseMirror') {
+      return <ProseMirrorEditor/>
+    }
+    else if (editorType === 'Markdown-it') {
+      return <div>placeholder for markdown-it component</div>
+    }
+    else {
+      return <div>ERROR</div>
+    } 
+  }
+
   return (
     <>
-      <ProseMirrorEditor/> 
+      <div>
+        <button onClick={() => setEditorType('TipTap')}>TipTap</button>
+        <button onClick={() => setEditorType('Markdown-it')}>Markdown-it</button>
+        <button onClick={() => setEditorType('ProseMirror')}>ProseMirror</button>
+      </div>
+      {getPreviewElement()}
     </>
   )
 }
