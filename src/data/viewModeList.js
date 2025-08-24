f4e1fa16109ad9e5b473f115ed93363149d015b3
Cruduper <briefcasemanx@gmail.com>
2025-08-24 15:05:52 -0700

    creates seperate file for defining view modes

---
+++ src/data/viewModeList.js
@@ -0,0 +1,16 @@
+export const DEFAULT = 'FORMATTED'
+
+export const viewModeList = {
+  FORMATTED: {
+    btnText: "Formatted",
+    headerText: "Formatted Preview",
+  },
+  HTML: {
+    btnText: "HTML",
+    headerText: "Raw HTML",
+  },
+  MARKDOWN: {
+    btnText: "Markdown",
+    headerText: "Raw Markdown",
+  }
+}
